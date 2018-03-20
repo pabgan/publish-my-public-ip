e503cb7f90bb5606d5ad4491e6986612526c6b29
Pablo Ganuza Vidal <pabloganuza@gmail.com>
2018-03-20 23:14:56 +0100

    Move some values to variables to make it easy to adapt to a new scenario.

--- publish_public_ip.sh
+++ publish_public_ip.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+output_machine="pablo@guaranuzas.com"
 output_folder="./inbox"
 hostname=$(hostname)
 filename=$hostname"_public_ip.txt"
+
 public_ip=$(curl ipinfo.io/ip)
 
 echo $public_ip > $filename
-scp $filename pablo@guaranuzas.com:$output_folder
+scp $filename $output_machine:$output_folder
